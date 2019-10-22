74575b473fb89af7c51ddf041f578b706da262c7
paulhfu <hiltpa@gmail.com>
2019-10-22 16:10:25 +0200

    deleted obsolete files

--- deriv_obj_v.m
+++
@@ -1,5 +0,0 @@
-function ret = deriv_obj_v(lambda, y, v, f, K, u, w, beta)
-    ret = lambda/2 .* (1/(y^2 + dotX((v-f), (v-f))) .* ones(size(f)))...
-        + beta * (K*u - v + w/beta)
-end
-
--- normXX.m
+++
@@ -1,5 +0,0 @@
-function out = normXX(arg)
-% returns a norm in XxX with X=R-NxN as
-    out = (arg(:,:,1).^2 + arg(:,:,2).^2).^(1/2);
-end
-
