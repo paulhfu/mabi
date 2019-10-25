fca418179f3d6d907fe39a446c3638ee69bb3108
paulhfu <hiltpa@gmail.com>
2019-10-25 17:46:02 +0200

    first working sol_n

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ clc; close all; clear all
 
 % call with some parameters
 img = randi(500);
-sigma = 0.01;
+sigma = 0.02;
 [b, u0] = denoisingLoadData('BSDS500', img, true, 'Cauchy', sigma);
 
 
@@ -23,40 +23,40 @@ u0 = u0(1:d, 1:d, :);
 b = b(1:d, 1:d, :);
 
 G = gradient_discrete_4('d', d);
-quality = 25;
+quality = 21;
 u_all = b;
-lbd = 1;
-y = 5;
-bet = round(0.005 + lbd / (y^2), 2); % ceil to nearest second decimal
-K = fspecial('gaussian', [9 9], 4);
-log1 = [];
-log2 = [];
+lbd = 0.008;
+y = 6;
+bet = 12;%round(0.005 + lbd / (y^2), 2)*40; % ceil to nearest second decimal
+
 for c=1:3
+    log1 = [-inf];
+    log2 = [-inf];
     f = squeeze(b(:,:,c));
     u = f;
     w = ones(size(f));
 
-    fnc = @(v_n) (lbd * (2*(v_n-f) ./ (y^2 + (v_n-f).^2)) - bet * (u - v_n + w/bet));
+    fnc = @(v_n) (lbd * ((v_n-f) ./ (y^2 + (v_n-f).^2)) - bet * (u - v_n + w/bet));
     fncdot = @(v_n) (lbd * (y^2-(v_n-f).^2) ./ (y^2 + (v_n-f).^2).^2 + bet * ones(size(v_n)));
     
     v = newton(fnc, fncdot, u, 0.1, 30);
     w = w + bet .* (u - v);
     
     log2 = [log2 PSNR(u0(:,:,c), u)];
-    while log2(end)<quality
+    
+    while log2(end)>log2(end-1) 
         g = v - w / bet;
         g = reshape(g,[size(g,1)*size(g,2) 1]);
         u = reshape(u,[size(u,1)*size(u,2) 1]);
         l = 1/bet;
         if log2(end) > 18
-            maxiter_d = 300;
-            maxiter_n = 100;
-% %             lbd = 1/4;
-        else
-            maxiter_d = 50;
+            maxiter_d = 10;
             maxiter_n = 10;
+        else
+            maxiter_d = 4;
+            maxiter_n = 4;
         end
-        u = denoising_anisotrop_tv(u, 'lambda', l, 'v', g, 'x0', u, 'maxIter', maxiter_d);
+        u = denoising_anisotrop_tv(u, 'lambda', l, 'v', g, 'maxIter', maxiter_d, 'l', 0 ,'u', 1);
         u = reshape(u,[d d]);
         
         fnc = @(v_n) (lbd * ((v_n-f) ./ (y^2 + (v_n-f).^2)) - bet * (u - v_n + w/bet));
@@ -81,7 +81,9 @@ for c=1:3
     end
     u_all(:,:,c) = u;
 end
-
+figure(3)
+subplot(1,2,1); imshow(u_all); title('denoised');
+subplot(1,2,2); imshow(b); title('Degraded');
 function ret = PSNR(u_true, u_pred)
     ret = 20 * log10(255 / normX(u_true-u_pred));
 end
